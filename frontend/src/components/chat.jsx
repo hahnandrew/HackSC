0f047c9ef23f7c6857d1959f9e0485e75379ec42
Jae Hahn <hahnandy0201@gmail.com>
2023-11-04 20:02:56 -0700

    fixing streamlit build issues

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -5,9 +5,10 @@ function PageComponent() {
   return (
     <div className="flex w-full h-full justify-center items-center bg-gray-100 py-10">
       <iframe
-        src="https://hahnandrew-hacksc-streamlitchat-oprizx.streamlit.app/"
+        // src="https://hahnandrew-hacksc-streamlitchat-oprizx.streamlit.app/"
+        src="http://localhost:8501/"
         title="Streamlit App"
-        width="100%"
+        width="50%"
         height="800px"
         style={{ border: "none" }}
       ></iframe>
