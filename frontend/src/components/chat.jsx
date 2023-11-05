74d3a654f26ecfd20682fddc942f6ed5f78cbda8
Jae Hahn <hahnandy0201@gmail.com>
2023-11-04 21:47:53 -0700

    fixed streamlit link

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -16,8 +16,8 @@ function PageComponent() {
   return (
     <div className="flex w-full h-full justify-center items-center bg-gray-100 py-10">
       <iframe
-        // src="https://hahnandrew-hacksc-streamlitchat-oprizx.streamlit.app/?user_token=${userToken}"
-        src={`http://localhost:8502/?user_token=${user.uid}`}
+        src="https://hacksc-fgg6nccbxyuqmxm4ceujpz.streamlit.app/?user_token=${userToken}"
+        // src={`http://localhost:8502/?user_token=${user.uid}`}
         title="Streamlit App"
         width="50%"
         height="800px"
