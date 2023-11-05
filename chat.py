4df205f81820e15162960242f719afde22fba96f
Juan Herrera <juanherrera@Juans-MBP.lan>
2023-11-04 18:06:03 -0700

    got rid of line 7 api key

--- chat.py
+++ chat.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
-# openai.api_key = st.secrets["OPENAI_API_KEY"]
-openai.api_key = "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"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+#openai.api_key = "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"
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
