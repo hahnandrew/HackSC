40d38339ca958764a47894955f0185d2d4fe3243
Jae Hahn <hahnandy0201@gmail.com>
2023-11-04 22:21:16 -0700

    added conditional logic for deployed n dev env

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -4,20 +4,20 @@ import { useAuth } from "../contexts/Authcontext";
 
 function PageComponent() {
 
+  const baseUrl = process.env.IS_DEPLOYED === 'true'
+    ? "https://hacksc-fgg6nccbxyuqmxm4ceujpz.streamlit.app"
+    : "http://localhost:8502";
+
   const { user } = useAuth()
 
-  // After authentication, send a message to the Streamlit app
-  // iframeRef.current.contentWindow.postMessage({
-  //   type: 'SET_USER',
-  //   data: { userToken: 'user-specific-token' }
-  // }, 'https://your-streamlit-app.com');
+  const queryParams = `?embed=true&user_token=${user.uid}`;
+  const iframeSrc = `${baseUrl}${queryParams}`;
 
 
   return (
     <div className="flex w-full h-full justify-center items-center bg-gray-100 py-10">
       <iframe
-        src="https://hacksc-fgg6nccbxyuqmxm4ceujpz.streamlit.app/?embed=true&user_token=${userToken}"
-        // src={`http://localhost:8502/?user_token=${user.uid}`}
+        src={iframeSrc}
         title="Streamlit App"
         width="50%"
         height="800px"
